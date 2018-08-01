31e55b6e37fa4a10d331b86d7a416ccb0ab830e1
McCoy <mccoybecker@gmail.com>
2018-08-01 16:27:32 -0700

    Quick sort

---
+++ qsort.hs
@@ -0,0 +1,7 @@
+-- Quick sort implemented in Haskell --
+
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+               where
+                    smaller = [a | a <- xs, a <= x]
+                    larger = [b | b <- xs, b > x]
